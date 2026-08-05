95642f57b522457b1e16c9f4e805e0298af08c5a
Sonilowx <olderdemo@gmail.com>
2026-08-05 11:10:02 +0300

    Add files via upload

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "velocity-socket-keeper",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "minecraft-protocol": "^1.42.0"
+  }
+}
